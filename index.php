a25d83d2e96675a20e372aa29c1a5695a4b9bebf
jakelinepenagos1 <70981425+jakelinepenagos1@users.noreply.github.com>
2021-09-13 16:53:57 -0500

    13

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="estilo.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+</head>
+<body class="bg-dark">
+
+<header></header>
+
+<main>
+
+<div class="container-fluid ">
+    <div class="row">
+
+    <div class="col-8">
+    <div class="row ms-2 mt-5 p-3 text-center">
+    <h1 class="titulo">BIENVENIDOS 😊 </h1>
+    <h4 class="titulo">juegos</h4>
+
+        <div class="col-4  p-3">
+        <label for="exampleFormControlInput1" class="form-label"></label>
+        <input type="number" class="form-control" id="" placeholder="">
+        </div>
+
+        <div class="col-4  p-3">
+        <label for="exampleFormControlInput1" class="form-label"></label>
+        <input type="number" class="form-control" id="" placeholder="">
+          </div>
+    
+        <div class="col-4  p-3">
+        <label for="exampleFormControlInput1" class="form-label"></label>
+       <input type="number" class="form-control" id="" placeholder="">
+       </div>
+       
+        <button type="button" class="btn btn-outline-success mt-5 p-3">enviar </button>
+    </div>
+
+ 
+
+    </div>
+
+    <div class="col-4 mt-5 p-3 align-self-center">
+
+    <img src="https://firebasestorage.googleapis.com/v0/b/juegos-cpc.appspot.com/o/imagen1.jpg?alt=media&token=fb0c2064-2708-443a-b8ae-dab746bc679b" alt="" class="img-fluid w-100 pa-2">
+    </div>
+
+
+    </div>
+</div>
+
+</main>
+
+<footer></footer>
+    
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+</body>
+
+</html>
