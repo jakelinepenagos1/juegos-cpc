b257646d8ba7fa699deaf764c1f1e72cbfcd00a9
jakelinepenagos1 <70981425+jakelinepenagos1@users.noreply.github.com>
2021-09-14 16:11:59 -0500

    14

--- index.php
+++ index.php
@@ -5,49 +5,58 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="estilo.css">
+    <link rel="stylesheet" href="hola.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 
 </head>
 <body class="bg-dark">
 
-<header></header>
+<header>
+
+</header>
 
 <main>
 
 <div class="container-fluid ">
     <div class="row">
 
-    <div class="col-8">
+    <div class="col-7">
     <div class="row ms-2 mt-5 p-3 text-center">
     <h1 class="titulo">BIENVENIDOS 😊 </h1>
     <h4 class="titulo">juegos</h4>
 
-        <div class="col-4  p-3">
-        <label for="exampleFormControlInput1" class="form-label"></label>
-        <input type="number" class="form-control" id="" placeholder="">
+        <div class="col p-3">
+        
+        <input type="number" class="form-control caja" id="numero1" placeholder="">
         </div>
 
-        <div class="col-4  p-3">
-        <label for="exampleFormControlInput1" class="form-label"></label>
-        <input type="number" class="form-control" id="" placeholder="">
+        <div class="col p-3">
+        
+        <input type="text" class="form-control caja text-center" id="operacion" value="+" readonly placeholder="">
           </div>
     
-        <div class="col-4  p-3">
-        <label for="exampleFormControlInput1" class="form-label"></label>
-       <input type="number" class="form-control" id="" placeholder="">
+        <div class="col p-3">
+        
+       <input type="number" class="form-control caja" id="numero2" placeholder="">
        </div>
+
+       <div class="col  p-3">
+        
+        <input type="text" class="form-control caja text-center" id="texto2" value="="  readonly placeholder="">
+          </div>
+
+          <div class="col  p-3">
+       
+        <input type="number" class="form-control caja " id="numero3" placeholder="">
+          </div>
        
         <button type="button" class="btn btn-outline-success mt-5 p-3">enviar </button>
     </div>
-
- 
-
     </div>
 
-    <div class="col-4 mt-5 p-3 align-self-center">
-
+    <div class="col-4 mt-5 p-3 align-self-center text-center">
     <img src="https://firebasestorage.googleapis.com/v0/b/juegos-cpc.appspot.com/o/imagen1.jpg?alt=media&token=fb0c2064-2708-443a-b8ae-dab746bc679b" alt="" class="img-fluid w-100 pa-2">
+    <button type="button" class="btn btn-outline-light mt-5 p-3 ">siguiente ☺😀</button>
     </div>
 
 
@@ -56,7 +65,9 @@
 
 </main>
 
-<footer></footer>
+<footer>
+
+</footer>
     
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
 </body>
